bc13f042a20019eff4127cf0e19660c8e3646f24
KingKaiZhuang <5b0g0024@stust.edu.tw>
2024-06-13 10:40:12 +0800

    Protecting Routes

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 import { baseUrl, postRequest } from "../utils/service";
 
 export const AuthContext = createContext();
@@ -13,7 +13,13 @@ export const AuthContextProvider = ({ children }) => {
     password: "",
   });
 
-  console.log("registerInfo", registerInfo);
+  console.log("User", user);
+
+  useEffect(() => {
+    const user = localStorage.getItem("User");
+
+    setUser(JSON.parse(user));
+  }, []);
 
   const updateRegisterInfo = useCallback((info) => {
     setRegisterInfo(info);

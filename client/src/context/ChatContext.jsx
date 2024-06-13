6f4d6cabd8fe01fe2f0cfa3b1d611683b5010b57
KingKaiZhuang <5b0g0024@stust.edu.tw>
2024-06-13 14:28:19 +0800

    feat: Add ChatContextProvider and update Chat page
    This commit adds the ChatContextProvider component to the App component in App.jsx, wrapping the NavBar and Container components. It also imports the ChatContext in the Chat page and uses it to access the userChats, isUserChatsLoading, and userChatsError variables. Additionally, a console.log statement is added to log the userChats data. These changes enable the Chat page to access and display chat-related information for the user.

---
+++ client/src/context/ChatContext.jsx
@@ -0,0 +1,41 @@
+import { useState, createContext, useEffect } from "react";
+import { baseUrl, getRequest, postRequest } from "../utils/service";
+
+export const ChatContext = createContext();
+
+export const ChatContextProvider = ({ children, user }) => {
+  const [userChats, setUserChats] = useState([]);
+  const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
+  const [userChatsError, setUserChatsError] = useState(null);
+
+  useEffect(() => {
+    const getUserChats = async () => {
+      if (user?._id) {
+        setIsUserChatsLoading(true);
+        setUserChatsError(null);
+        const response = await getRequest(`${baseUrl}/chats/${user?._id}`);
+
+        setIsUserChatsLoading(false);
+
+        if (response.error) {
+          return setUserChatsError(response);
+        }
+
+        setUserChats(response);
+      }
+    };
+    getUserChats();
+  }, [user]);
+
+  return (
+    <ChatContext.Provider
+      value={{
+        userChats,
+        isUserChatsLoading,
+        userChatsError,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
